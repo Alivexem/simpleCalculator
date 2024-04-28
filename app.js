303e6f80e51710ab4c23c24f5014d45f520d97d7
Athkins Emmanuel Testimony <athkinstestimony@gmail.com>
2024-04-28 09:35:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ let display= document.getElementById("display")
 let button = document.querySelectorAll(".btn")
 button.forEach((item) => {
     item.addEventListener("click",() => {
+        if(item.innerText == "del"){
+            let displayValue = display.value.split("")
+            displayValue.pop()
+            let newDisplay = displayValue.join("")
+            display.value = newDisplay
+            return
+        }
         if(item.innerText === "="){            
             try{                
                 let result = parseFloat(eval(display.value))          
